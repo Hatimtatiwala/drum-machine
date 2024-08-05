5aac68244f874f701bbce37035d7344434f49c66
hatimtatiwala <hatimalitati@gmail.com>
2024-08-05 15:33:04 +0530

    Updates

---
+++ precache-manifest.3cbcd564a00889a88b8eed7405f21471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b477f6b412e644801b52c747ff24f289",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "5c73a795104e49a5e4a9",
+    "url": "/drum-machine/static/css/main.1a63702d.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/drum-machine/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/drum-machine/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "5c73a795104e49a5e4a9",
+    "url": "/drum-machine/static/js/main.982e75d8.chunk.js"
+  },
+  {
+    "revision": "2ea57db823665fb24985",
+    "url": "/drum-machine/static/js/runtime~main.99f3c123.js"
+  }
+]);
